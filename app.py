5d621c39a2bce5c6e4d4a5c58f34349fc4d0578b
Awesome-DC <confirmverfi04@gmail.com>
2026-02-21 01:54:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ app = Flask(__name__)
 CORS(app, origins=[
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    'http://norman-earn.vercel.app/'
     # 'https://yourdomain.com',  # ← add your real domain here when deployed
 ])
 
